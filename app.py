084b2c567c4152d36c2fbb40db201591edf9d7c3
biichou22 <belkhiribachir16@gmail.com>
2018-12-16 11:09:06 +0100

    Added PostgreSQL environment and psycopg2 module

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # />python3 -m pip install Flask
 # />python3 -m pip install Flask-RESTful #For RESTful API
 # />python3 -m pip install Flask-JWT #JWT JSON WEB TOOL For Obfuscation
-
+import os
 from flask import Flask
 from flask_restful import Resource, Api
 from flask_jwt import JWT
@@ -12,7 +12,11 @@ from resources.store import Store, StoreList
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db' # For sqlite. You can use others like MYSQL, PostgrSQL without changing the code just this line
+# For sqlite. You can use others like MYSQL, PostgrSQL without changing the code just this line
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db' 
+# After deploying it to Heroku we need to use the postgres to not loose the data.
+# So we use os to get the environement variable that matches the postgres database URL or the sqlite db url for backup
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','sqlite:///data.db') 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # encryption key
 app.secret_key = "bachir"
